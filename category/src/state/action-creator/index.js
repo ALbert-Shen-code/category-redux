f6b5014a17439555a8504cf24f5fcd12050e0bfb
ALbert-Shen-code <siyuanshen1993@gmail.com>
2021-08-31 14:26:25 +1000

    check and sort the array

--- category/src/state/action-creator/index.js
+++ category/src/state/action-creator/index.js
@@ -4,14 +4,24 @@ export const getCategories = () => async (dispatch) => {
     const { data } = await api.fetchData();
     let rootList = data.filter((rootItem) => rootItem.parent === "root");
 
+    rootList = rootList.sort((a, b) => {
+      let m = a.categoryId.slice(-1);
+      let n = b.categoryId.slice(-1);
+      return m - n;
+    });
+
     rootList.map((item) => {
       item.children = [];
       const hasChild = (t) => {
         let child = data.filter((s) => s.parent === t.categoryId);
         t.children = child;
-        t.value = t.name;
-        t.title = t.categoryId;
-
+        t.value = t.categoryId;
+        t.title = t.name;
+        t.children = t.children.sort((a, b) => {
+          let m = a.categoryId.slice(-1);
+          let n = b.categoryId.slice(-1);
+          return m - n;
+        });
         //call the function again if the children array is not empty
         if (t.children.length > 0) {
           t.children.map((gs) => {
